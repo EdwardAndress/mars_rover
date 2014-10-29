38a94e59875461372b2ee4dfc3053c29f1ea1794
edward andress <eddie_andress@hotmail.com>
2014-10-29 10:42:28 +0000

    made some methods private on master branch version

--- lib/rover.rb
+++ lib/rover.rb
@@ -21,6 +21,79 @@ class Rover #used to explore a rectangular plateau and can be fed a list of comm
 		end
 		report
 	end
+	
+	def explore
+
+		move_to_south_western_corner
+
+		until positions_log.count == total_plateau_area
+			face_opposite_edge
+			(plateau_size[1]).times { move }
+			change_heading_to('E')
+			move
+		end
+	end
+
+	def turn_right #used to add 1 to numberical heading
+		heading_alphabetical((current_heading_numerical) + 1)
+	end
+
+	def turn_left #used to subtract 1 from numerical heading
+		heading_alphabetical((current_heading_numerical) - 1)
+	end
+
+	def change_heading_to(new_heading) #change heading by passing N,S,E or W as a string
+		@heading = new_heading
+	end
+
+	def accept_plateau_data(size) #takes plateau size as two numbers separated by a space in a string eg . "5 5"
+		@plateau_size = size.split(" ").map {| dimension | dimension.to_i}
+	end
+
+	def safe_move_north? #checks whether rover will fall off the plateau by moving North
+		@position[1] < @plateau_size[1]
+	end
+
+	def safe_move_south? #checks whether rover will fall off the plateau by moving South
+		@position[1] > 0
+	end
+
+	def safe_move_east? #checks whether rover will fall off the plateau by moving East
+		@position[0] < @plateau_size[0]
+	end
+
+	def safe_move_west? #checks whether rover will fall off the plateau by moving West
+		@position[0] > 0
+	end
+
+	def move
+		if 		heading == 'N' && safe_move_north?
+					move_north
+					@positions_log << report
+		elsif 	heading == 'S' && safe_move_south?
+					move_south
+					@positions_log << report
+		elsif 	heading == 'E' && safe_move_east?
+					move_east
+					@positions_log << report 					
+		elsif 	heading == 'W' && safe_move_west?
+					move_west
+					@positions_log << report
+		else
+			@errors << {Time.now => "Invalid move (aborted) at #{@position} heading #{heading}"}
+		end
+	end
+
+	def report
+		"#{position.join(" ")} #{heading}" #reports position coordinates and heading for 'output'
+	end
+
+
+	private
+
+	def new_heading_from(instruction) #converts 'L' or 'R' turn instructions into a new heading
+		instruction == 'R' ? turn_right : turn_left
+	end
 
 	def move_to_south_western_corner
 		move_to_southern_edge
@@ -68,17 +141,6 @@ class Rover #used to explore a rectangular plateau and can be fed a list of comm
 		end	
 	end
 
-	def explore
-
-		move_to_south_western_corner
-
-		until positions_log.count == total_plateau_area
-			face_opposite_edge
-			(plateau_size[1]).times { move }
-			change_heading_to('E')
-			move
-		end
-	end
 
 	def current_heading_numerical #converts N/S/E/W heading to a numerical value for manipulation by turn instructions
 		["N", "E", "S", "W", "N"].index(heading)
@@ -86,43 +148,6 @@ class Rover #used to explore a rectangular plateau and can be fed a list of comm
 
 	def heading_alphabetical(heading_numerical) #converts a numerical value into N/S/E/W heading - does not use current heading, it uses 
 		heading_numerical == -1 ? 'W' : ["N", "E", "S", "W", "N"][heading_numerical]
-	end
-
-	def turn_right #used to add 1 to numberical heading
-		heading_alphabetical((current_heading_numerical) + 1)
-	end
-
-	def turn_left #used to subtract 1 from numerical heading
-		heading_alphabetical((current_heading_numerical) - 1)
-	end
-
-	def new_heading_from(instruction) #converts 'L' or 'R' turn instructions into a new heading
-		instruction == 'R' ? turn_right : turn_left
-	end
-
-
-	def change_heading_to(new_heading) #change heading by passing N,S,E or W as a string
-		@heading = new_heading
-	end
-
-	def accept_plateau_data(size) #takes plateau size as two numbers separated by a space in a string eg . "5 5"
-		@plateau_size = size.split(" ").map {| dimension | dimension.to_i}
-	end
-
-	def safe_move_north? #checks whether rover will fall off the plateau by moving North
-		@position[1] < @plateau_size[1]
-	end
-
-	def safe_move_south? #checks whether rover will fall off the plateau by moving South
-		@position[1] > 0
-	end
-
-	def safe_move_east? #checks whether rover will fall off the plateau by moving East
-		@position[0] < @plateau_size[0]
-	end
-
-	def safe_move_west? #checks whether rover will fall off the plateau by moving West
-		@position[0] > 0
 	end
 
 	def move_north
@@ -139,28 +164,6 @@ class Rover #used to explore a rectangular plateau and can be fed a list of comm
 
 	def move_west
 		@position[0] -= 1
-	end
-
-	def move
-		if 		heading == 'N' && safe_move_north?
-					move_north
-					@positions_log << report
-		elsif 	heading == 'S' && safe_move_south?
-					move_south
-					@positions_log << report
-		elsif 	heading == 'E' && safe_move_east?
-					move_east
-					@positions_log << report 					
-		elsif 	heading == 'W' && safe_move_west?
-					move_west
-					@positions_log << report
-		else
-			@errors << {Time.now => "Invalid move (aborted) at #{@position} heading #{heading}"}
-		end
-	end
-
-	def report
-		"#{position.join(" ")} #{heading}" #reports position coordinates and heading for 'output'
 	end
 
 end
